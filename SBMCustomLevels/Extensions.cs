0ce90b018dbbb7a051a0c62093a6c996631cf487
flarfo <noahwhelden@gmail.com>
2023-10-05 12:01:56 -0400

    created Extensions.cs

---
+++ SBMCustomLevels/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SBM_CustomLevels.Extensions
+{
+    public static class Extensions
+    {
+        public static bool TryGetComponentInParent<T>(this Transform gameObject, out T component) where T : Component
+        {
+            component = gameObject.GetComponentInParent<T>();
+
+            if (component)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
